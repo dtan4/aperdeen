029265aaeef9e53808bc234708bdff31f20685e3
Daisuke Fujita <dtanshi45@gmail.com>
2017-10-25 16:04:30 +0900

    Take root option and enable pretty error print

--- cmd/root.go
+++ cmd/root.go
@@ -13,17 +13,29 @@ var RootCmd = &cobra.Command{
 	Short: "Amazon API Gateway client and local proxy",
 }
 
+var rootOpts = struct {
+	debug  bool
+	region string
+}{}
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		if rootOpts.debug {
+			fmt.Printf("%+v\n", err)
+		} else {
+			fmt.Println(err)
+		}
 		os.Exit(1)
 	}
 }
 
 func init() {
 	cobra.OnInitialize(initConfig)
+
+	RootCmd.PersistentFlags().BoolVar(&rootOpts.debug, "debug", false, "Debug mode")
+	RootCmd.PersistentFlags().StringVar(&rootOpts.region, "region", "", "AWS region")
 }
 
 // initConfig reads in config file and ENV variables if set.
