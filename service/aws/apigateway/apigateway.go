ea298b8e458af4797cb9017ae195ee217b679bfb
Daisuke Fujita <dtanshi45@gmail.com>
2017-10-25 14:26:54 +0900

    Retrieve registered API list

--- service/aws/apigateway/apigateway.go
+++ service/aws/apigateway/apigateway.go
@@ -1,8 +1,21 @@
 package apigateway
 
 import (
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/apigateway"
 	"github.com/aws/aws-sdk-go/service/apigateway/apigatewayiface"
+	"github.com/pkg/errors"
 )
+
+// API represents the wrapped form of RestApi
+type API struct {
+	ID          string
+	Name        string
+	Description string
+	CreatedDate time.Time
+}
 
 // Client represents the wrapper of Amazon API Gateway API client
 type Client struct {
@@ -14,4 +27,25 @@ func NewClient(api apigatewayiface.APIGatewayAPI) *Client {
 	return &Client{
 		api: api,
 	}
+}
+
+// ListAPIs returns the list of registered APIs
+func (c *Client) ListAPIs() ([]*API, error) {
+	resp, err := c.api.GetRestApis(&apigateway.GetRestApisInput{})
+	if err != nil {
+		return []*API{}, errors.Wrap(err, "cannot retrieve registered APIs")
+	}
+
+	apis := []*API{}
+
+	for _, item := range resp.Items {
+		apis = append(apis, &API{
+			ID:          aws.StringValue(item.Id),
+			Name:        aws.StringValue(item.Name),
+			Description: aws.StringValue(item.Description),
+			CreatedDate: aws.TimeValue(item.CreatedDate),
+		})
+	}
+
+	return apis, nil
 }
--- service/aws/apigateway/apigateway_test.go
+++ service/aws/apigateway/apigateway_test.go
@@ -1,8 +1,13 @@
 package apigateway
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
+	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/apigateway"
 	"github.com/golang/mock/gomock"
 )
 
@@ -15,5 +20,79 @@ func TestNewClient(t *testing.T) {
 	client := NewClient(api)
 	if client.api != api {
 		t.Error("invalid client")
+	}
+}
+
+func TestListAPIs(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	api := NewMockAPIGatewayAPI(ctrl)
+	api.EXPECT().GetRestApis(&apigateway.GetRestApisInput{}).Return(&apigateway.GetRestApisOutput{
+		Items: []*apigateway.RestApi{
+			&apigateway.RestApi{
+				Id:          aws.String("abcde12345"),
+				Name:        aws.String("example"),
+				Description: aws.String("example APIs"),
+				CreatedDate: aws.Time(time.Date(2017, 10, 25, 12, 34, 56, 0, time.UTC).UTC()),
+			},
+			&apigateway.RestApi{
+				Id:          aws.String("12345abcde"),
+				Name:        aws.String("foobar"),
+				Description: aws.String("foobar APIs"),
+				CreatedDate: aws.Time(time.Date(2017, 10, 25, 12, 00, 00, 0, time.UTC).UTC()),
+			},
+		},
+	}, nil)
+	client := &Client{
+		api: api,
+	}
+
+	got, err := client.ListAPIs()
+	if err != nil {
+		t.Errorf("got error: %s", err)
+		return
+	}
+
+	want := []*API{
+		&API{
+			ID:          "abcde12345",
+			Name:        "example",
+			Description: "example APIs",
+			CreatedDate: time.Date(2017, 10, 25, 12, 34, 56, 0, time.UTC).UTC(),
+		},
+		&API{
+			ID:          "12345abcde",
+			Name:        "foobar",
+			Description: "foobar APIs",
+			CreatedDate: time.Date(2017, 10, 25, 12, 00, 00, 0, time.UTC).UTC(),
+		},
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %#v, want: %#v", got, want)
+	}
+}
+
+func TestListAPIs_error(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	api := NewMockAPIGatewayAPI(ctrl)
+	api.EXPECT().GetRestApis(&apigateway.GetRestApisInput{}).Return(&apigateway.GetRestApisOutput{}, fmt.Errorf("error"))
+	client := &Client{
+		api: api,
+	}
+
+	_, err := client.ListAPIs()
+	if err == nil {
+		t.Errorf("got no error")
+		return
+	}
+
+	want := "cannot retrieve registered APIs: error"
+
+	if err.Error() != want {
+		t.Errorf("got: %#v, want: %#v", err.Error(), want)
 	}
 }
