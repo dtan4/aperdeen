984d27eb4e9e41cfba2b031d2c09340d1c7fff7e
Daisuke Fujita <dtanshi45@gmail.com>
2017-10-27 16:31:06 +0900

    Implement local proxy

---
+++ proxy/proxy.go
@@ -0,0 +1,64 @@
+package proxy
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+
+	"github.com/dtan4/aperdeen/model"
+	"github.com/gorilla/mux"
+	"github.com/pkg/errors"
+)
+
+var availableSchemas = map[string]bool{
+	"http":  true,
+	"https": true,
+}
+
+// CreateProxyHandler creates local API Gateway server
+func CreateProxyHandler(endpoints map[string]*model.Endpoint) (http.Handler, error) {
+	r := mux.NewRouter().StrictSlash(true)
+
+	for _, ep := range endpoints {
+		target, err := url.Parse(ep.TargetURL)
+		if err != nil {
+			return nil, errors.Wrapf(err, "cannot parse %q as URL", ep.TargetURL)
+		}
+
+		if _, ok := availableSchemas[target.Scheme]; !ok {
+			return nil, errors.Errorf("invalid URI schema: %s", target.Scheme)
+		}
+
+		d := director(target)
+
+		// add "/foo/{rest:.*}" route from "/foo/*"
+		r.Handle(strings.Replace(ep.Path, "*", "{rest:.*}", -1), &httputil.ReverseProxy{
+			Director: d,
+		})
+
+		// add "/foo" route from "/foo/*"
+		if strings.HasSuffix(ep.Path, "/*") {
+			r.Handle(strings.TrimSuffix(ep.Path, "/*"), &httputil.ReverseProxy{
+				Director: d,
+			})
+		}
+	}
+
+	return r, nil
+}
+
+func director(target *url.URL) func(*http.Request) {
+	return func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = strings.Replace(target.Path, "*", mux.Vars(req)["rest"], -1)
+
+		targetQuery := target.RawQuery
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+	}
+}
---
+++ proxy/proxy_test.go
@@ -0,0 +1,135 @@
+package proxy
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/dtan4/aperdeen/model"
+	"github.com/gorilla/mux"
+)
+
+func TestCreateProxyHandler(t *testing.T) {
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}).Methods("GET")
+	r.HandleFunc("/bar", func(w http.ResponseWriter, req *http.Request) {
+		switch req.Method {
+		case "GET":
+			w.WriteHeader(http.StatusOK)
+		case "POST":
+			w.WriteHeader(http.StatusCreated)
+		}
+	}).Methods("GET", "POST")
+	r.HandleFunc("/error", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}).Methods("GET")
+	r.HandleFunc("/ops/ping", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}).Methods("GET")
+	// dummy endpoint!
+	r.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}).Methods("GET")
+
+	origin := httptest.NewServer(r)
+	defer origin.Close()
+
+	endpoints := map[string]*model.Endpoint{
+		"/foo/ping": &model.Endpoint{
+			Path:      "/foo/ping",
+			TargetURL: fmt.Sprintf("%s/ops/ping", origin.URL),
+		},
+		"/foo/*": &model.Endpoint{
+			Path:      "/foo/*",
+			TargetURL: fmt.Sprintf("%s/*", origin.URL),
+		},
+	}
+
+	handler, err := CreateProxyHandler(endpoints)
+	if err != nil {
+		t.Errorf("got error: %s", err)
+		return
+	}
+
+	s := httptest.NewServer(handler)
+	defer s.Close()
+
+	ru, err := url.Parse(s.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testcases := []struct {
+		method string
+		path   string
+		want   int
+	}{
+		// nonexist endpoints
+		{
+			method: "GET",
+			path:   "/bar/",
+			want:   http.StatusNotFound,
+		},
+
+		{
+			method: "GET",
+			path:   "/baz/",
+			want:   http.StatusNotFound,
+		},
+		// endpoint exists
+		{
+			method: "GET",
+			path:   "/foo",
+			want:   http.StatusOK,
+		},
+		{
+			method: "GET",
+			path:   "/foo/",
+			want:   http.StatusOK,
+		},
+		{
+			method: "GET",
+			path:   "/foo/bar",
+			want:   http.StatusOK,
+		},
+		{
+			method: "POST",
+			path:   "/foo/bar",
+			want:   http.StatusCreated,
+		},
+		{
+			method: "GET",
+			path:   "/foo/error",
+			want:   http.StatusInternalServerError,
+		},
+		{
+			method: "GET",
+			path:   "/foo/ping",
+			want:   http.StatusOK,
+		},
+	}
+
+	c := &http.Client{}
+
+	for _, tc := range testcases {
+		ru.Path = tc.path
+		req := &http.Request{
+			Method: tc.method,
+			URL:    ru,
+		}
+
+		resp, err := c.Do(req)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+
+		if resp.StatusCode != tc.want {
+			t.Errorf("method: %s, path: %s, want: %d, got: %d", tc.method, tc.path, tc.want, resp.StatusCode)
+		}
+	}
+}
